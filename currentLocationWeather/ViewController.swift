0d7b7295489ddedc37c3023b9aadc8006f328090
sapaGit <mosevaly@gmail.com>
2022-06-09 11:10:24 +0300

    01.3 setup location

--- currentLocationWeather/ViewController.swift
+++ currentLocationWeather/ViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
 
     @IBOutlet var tableView: UITableView!
     
     var models = [Weather]()
+    
+    let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +29,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupLocation()
+    }
+    //Location
+    
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
+    
+    func requestWeatherForLocation() {
+        guard let currentLocation = currentLocation else { return }
+
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        print("\(long), \(lat)")
+    }
+    
+    // Table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
