4b35e8451c99aac1e7b49519e2ffed887350a647
sapaGit <mosevaly@gmail.com>
2022-06-09 10:35:35 +0300

    01.1 Customize ViewController

--- currentLocationWeather/ViewController.swift
+++ currentLocationWeather/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet var tableView: UITableView!
+    
+    var models = [Weather]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Register 2 cells
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
 
-
+struct Weather {
+    
 }
 
